de8dbd5159cebd71078bde76f3c3e9b939cb6ff8
EgOlga <workforolga@inbox.ru>
2024-06-10 23:41:25 +0300

    Add files via upload
    Егорова Ольга "Строки и индексация строк"

---
+++ main1.py
@@ -0,0 +1,9 @@
+#Егорова Ольга Строки и индексация строк
+
+example="Обучение"
+print(example[0])
+print(example[-1])  #print(len(example))
+print(example[3:])
+print(example[::-1])
+print(example[-1])
+print(example[1::2])
