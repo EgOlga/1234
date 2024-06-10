d4cd21d08d3b1c0fa9b5b37012c6c18a77257dcd
EgOlga <workforolga@inbox.ru>
2024-06-10 21:52:59 +0300

    Add files via upload
    Практика "Базовые структуры данных"

---
+++ main.py
@@ -0,0 +1,32 @@
+#Егорова Ольга
+#Длина слова
+a = 'Hello, Denis!'
+print(len(a))
+
+#Суммы и разности
+first=35
+second=27
+summa=first+second
+diff=first-second
+print(summa)
+print(diff)
+
+#Среднее арифметическое
+first=35
+second=27
+third=76
+mean=(first+second+third)//3
+print(mean)
+
+#Простые строчки
+first_string=("Вторник")
+second_string=("Понедельник")
+print(second_string + ", " + first_string)
+
+#Сложная формула
+a=7
+b=10
+c=21
+f=(a*b)+(a*c)
+#print(f)
+print((f**3)/2)
