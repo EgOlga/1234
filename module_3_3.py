0177d6f6472fb3a54641d3d0abdf5334344d1bb5
EgOlga <workforolga@inbox.ru>
2024-07-08 18:10:16 +0300

    Update module_3_3.py
    Здравствуйте.
    добавлен словарь в решение

--- module_3_3.py
+++ module_3_3.py
@@ -9,13 +9,12 @@ print_params(10, True)
 print_params(b = 25)
 print_params(c = [1, 2, 3])
 
+
+values_list = [5, True, 'Солнце']
+values_dict = {"a":1, "b":'Строка', "c":True}
+print_params(*values_list)
+print_params(**values_dict)
+
+
 values_list_2 = [5, 'Urban']
 print_params(*values_list_2, 42)
-
-
-
-def append_to_list(item, list_my=None):
-    if list_my is None:
-        list_my = []
-    list_my.append(item)
-print(list_my)
