0a9b0cda4af13ac1934171350bbf194fea219d1b
Olga <workforolga@inbox.ru>
2024-10-13 14:33:21 +0300

    Add files via upload
        def watch_video(self, video):
            if self.current_user and self.current_user.age < 18:
                print('Вам нет 18 лет, пожалуйста, покиньте страницу')
            elif self.current_user:
                for i in self.videos:
                    if video in i.title:
                        for j in range(1, 11):
                            print(j, end=' ')
                            time.sleep(1)
                        print('Конец видео')
            else:
                print('Войдите в аккаунт, чтобы смотреть видео')

--- module_5hard.py
+++ module_5hard.py
@@ -40,18 +40,19 @@ class UrTube:
                 list_movie.append(video.title)
         return list_movie
 
-    def watch_video(self, movie):
+    def watch_video(self, video):
         if self.current_user and self.current_user.age < 18:
             print('Вам нет 18 лет, пожалуйста, покиньте страницу')
         elif self.current_user:
-            for video in self.videos:
-                if movie in video.title:
-                    for i in range(1, 11):
-                        print(i, end=' ')
+            for i in self.videos:
+                if video in i.title:
+                    for j in range(1, 11):
+                        print(j, end=' ')
                         time.sleep(1)
                     print('Конец видео')
         else:
             print('Войдите в аккаунт, чтобы смотреть видео')
+
 
     def __str__(self):
         return f'{self.videos}'
