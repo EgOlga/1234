9e48cf4dc55dbf75ca68ae7eb7e3bb56082ba002
Olga <workforolga@inbox.ru>
2024-10-16 21:09:32 +0300

    Add files via upload

---
+++ tests_12_2.py
@@ -0,0 +1,47 @@
+import runner_and_tournament
+import unittest
+
+
+class TournamentTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.runer_1 = runner_and_tournament.Runner('Усэйн', 10)
+        self.runer_2 = runner_and_tournament.Runner('Андрей', 9)
+        self.runer_3 = runner_and_tournament.Runner('Ник', 3)
+
+
+    @classmethod
+    def tearDownClass(cls):
+        for test_key, test_value in cls.all_results.items():
+            print(f'Тест: {test_key}')
+            for key, value in test_value.items():
+                print(f'\t{key}: {value.name}')
+
+
+
+    def test_turn1(self):
+        turn_1 = runner_and_tournament.Tournament(90, self.runer_1, self.runer_3)
+        result = turn_1.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.all_results['test_turn1'] = result
+
+    def test_turn2(self):
+        turn_2 = runner_and_tournament.Tournament(90, self.runer_2, self.runer_3)
+        result = turn_2.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.all_results['test_turn2'] = result
+
+    def test_turn3(self):
+        turn_3 = runner_and_tournament.Tournament(90, self.runer_1, self.runer_2, self.runer_3)
+        result = turn_3.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.all_results['test_turn3'] = result
+
+
+
+    if __name__ == '__main__':
+        unittest.main()
