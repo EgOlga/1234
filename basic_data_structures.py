3aa812bee6a2408463f0fe15dd4f7e4278befa6b
Olga <workforolga@inbox.ru>
2025-01-21 21:50:34 +0300

    Add files via upload

---
+++ basic_data_structures.py
@@ -0,0 +1,14 @@
+#"1st program"
+print((9*5)**0,5)
+
+#"2nd program"
+print(9.99>9.99 and 9.99>1000 or 9.99!=1000.1)
+
+#"3rd program"
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2)))
+
+#"4th program"
+str1 = '123.456'
+print(int((float(str1)*10)%10))
